55bd16f61158ca9eb03296abd3266b18285c7dd3
Capi03 <cinthiacarrasco321@gmail.com>
2025-10-01 04:03:59 -0400

    ACTUALIZACIONES

--- src/pages/usuarios/service.ts
+++ src/pages/usuarios/service.ts
@@ -1,39 +1,34 @@
 import { AppUser, CreateUserPayload } from "./types";
 import { fetchJson } from "../../shared/api";
-//const API_BASE = import.meta.env.VITE_API_URL;
-
 
 // --- Usuarios --- //
 export function listarUsuarios(): Promise<AppUser[]> {
-  return fetchJson<AppUser[]>("/usuarios/");
+  return fetchJson<AppUser[]>("/users/");
 }
 
 export function crearUsuario(payload: CreateUserPayload): Promise<AppUser> {
-  return fetchJson<AppUser>("/usuarios/", {
+  return fetchJson<AppUser>("/users/", {
     method: "POST",
     body: JSON.stringify(payload),
-    csrf: true,
   });
 }
 
 export function actualizarUsuario(id: number, payload: Partial<CreateUserPayload>): Promise<AppUser> {
-  return fetchJson<AppUser>(`/usuarios/${id}/`, {
+  return fetchJson<AppUser>(`/users/${id}/`, {
     method: "PATCH",
     body: JSON.stringify(payload),
-    csrf: true,
   });
 }
 
 export function actualizarFoto(id: number, file: File): Promise<AppUser> {
   const fd = new FormData();
   fd.append("photo", file);
-  return fetchJson<AppUser>(`/usuarios/${id}/foto/`, {
+  return fetchJson<AppUser>(`/users/${id}/foto/`, {
     method: "POST",
     body: fd,
-    csrf: true,
   });
 }
 
 export function eliminarUsuario(id: number): Promise<void> {
-  return fetchJson<void>(`/usuarios/${id}/`, { method: "DELETE", csrf: true });
+  return fetchJson<void>(`/users/${id}/`, { method: "DELETE" });
 }
